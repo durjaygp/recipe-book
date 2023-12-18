1bc2249ff817c8a7136e81549f93880a2ff6ac67
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-12-18 19:23:31 +0600

    ddd

--- resources/views/frontEnd/home/index.blade.php
+++ resources/views/frontEnd/home/index.blade.php
@@ -35,6 +35,16 @@
                         <div class="text">
                             <p>{{$about->description}}</p>
                         </div>
+                        <div class="main-footer">
+                            <ul class="social-box">
+                                <li><a href="#"><span class="fa fa-pinterest-p"></span></a></li>
+                                <li><a href="#"><span class="fa fa-facebook-f"></span></a></li>
+                                <li><a href="#"><span class="fa fa-instagram"></span></a></li>
+                                <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                                <li><a href="#"><span class="fa fa-youtube-play"></span></a></li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
 
